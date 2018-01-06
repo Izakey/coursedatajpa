2fb3f52f471e9deff4292eb7fe0dc489917db85e
Awasum Yannick <awasum.yannick@skylabase.com>
2018-01-06 13:46:31 +0100

    Fix beans not found exception on Topic Repository

--- src/main/java/io/isaac/springbootstarter/topics/TopicRepository.java
+++ src/main/java/io/isaac/springbootstarter/topics/TopicRepository.java
@@ -1,8 +1,6 @@
 package io.isaac.springbootstarter.topics;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TopicRepository extends CrudRepository<Topic, String> {
 }
