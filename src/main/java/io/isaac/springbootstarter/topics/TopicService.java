2e5cdc6afab11e939e392b217663f6bd2362d22e
Awasum Yannick <yannickawasum@gmail.com>
2018-01-06 14:15:42 +0100

    Fix beans not found exception on Topic Repository (#1)
    Merge commit from Awesome Awasum

--- src/main/java/io/isaac/springbootstarter/topics/TopicService.java
+++ src/main/java/io/isaac/springbootstarter/topics/TopicService.java
@@ -1,15 +1,13 @@
 package io.isaac.springbootstarter.topics;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 
-    @Autowired
     private TopicRepository topicRepository;
 
     public List<Topic> getAllTopics(){
